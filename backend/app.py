459b189cc799c03daa3c812c47b14b6580293142
rohaan098 <51942524+rohaan098@users.noreply.github.com>
2025-07-25 18:16:30 +0500

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -45,5 +45,5 @@ def reviews():
     })
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
